7f27a3e512abbd8217c37976f52367b9b6f8c2e7
niranjanbaviskar <niranjanbaviskar23@gmail.com>
2024-08-24 16:25:28 +0530

    first commit

---
+++ src/components/home/Footer/FooterListTitle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterListTitle = ({ title }) => {
+  return <h3 className="text-xl font-bodyFont font-semibold mb-6"></h3>;
+};
+
+export default FooterListTitle;
